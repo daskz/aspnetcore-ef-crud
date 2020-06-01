033f34d464d9a9c1f1297a1abc457945e81c7531
Dastan S <tdaskz@gmail.com>
2020-06-01 18:15:26 +0600

    Added unit tests for RequestNumberProvider

---
+++ Dotnetvue.Tests/RequestNumberProviderTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Dotnetvue.Data.Models;
+using Dotnetvue.Web.Services;
+using Xunit;
+
+namespace Dotnetvue.Tests
+{
+    public class RequestNumberProviderTests
+    {
+        [Fact]
+        public void GenerateCorrectNumberTest()
+        {
+            var provider = new RequestNumberProvider();
+            var financeRequest = new FinanceRequest
+            {
+                Id = null,
+                Purpose = "TEST PURPOSE",
+                Amount = 5000000,
+                MonthCount = 36,
+                InterestRate = 15
+            };
+            
+            var dateString = DateTime.Now.ToString("ddMMyyyyhhmmss");
+            var number = provider.Generate(financeRequest);
+
+            Assert.Equal($"KZ{dateString}{financeRequest.GetHashCode()}", number);
+        }
+    }
+}
