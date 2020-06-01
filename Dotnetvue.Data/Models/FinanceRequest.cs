d3eaadcc9a6004ab77eac2544b69c2c88632a111
Dastan S <tdaskz@gmail.com>
2020-06-01 18:13:04 +0600

    Added DbContext with FinanceRequest entity

---
+++ Dotnetvue.Data/Models/FinanceRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Dotnetvue.Data.Models
+{
+    public class FinanceRequest
+    {
+        public Guid? Id { get; set; }
+        public string PublicNumber { get; set; }
+
+        public string Purpose { get; set; }
+
+        public int MonthCount { get; set; }
+
+        [Range(1, 100)]
+        public int InterestRate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        [JsonIgnore]
+        public User Author { get; set; }
+    }
+}
